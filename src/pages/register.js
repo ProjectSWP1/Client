c927233fa626a96bb09e353ecdbfe7d368ef6e29
Đào Lan Ngọc <kimtaehyung2110@gmail.com>
2023-09-27 16:17:49 +0700

    Sign Up First change

---
+++ src/pages/register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignUp from '../components/Register/SignUp.js';
+
+const Membership = () => {
+	return (
+		<div>
+			<SignUp/>
+		</div>
+	);
+};
+
+export default Membership;
