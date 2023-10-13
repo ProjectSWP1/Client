8b2e6b17cb549c4f8621f66ca2c15f39b3c87ae1
DaoLanNgoc <kimtaehyung2110@gmail.com>
2023-10-13 15:08:55 +0700

    Update verified email

--- src/components/Register/VerifyEmail.js
+++ src/components/Register/VerifyEmail.js
@@ -1,18 +1,31 @@
-import React from 'react'
 import './VerifyEmail.css'
-import { useParams } from 'react-router-dom'
+import { Button } from '@mui/material'
+import SignIn from '../Login/SignIn.js'
+import React, { Component } from 'react'
 
-export default function VerifyEmail() {
-  let { email } = useParams()
-  return (
-    <div className='verify-container'>
-      <div className='verify'>
-        <p>Enter your OTP: </p>
-        <form className='verify-form'>
-          <input type='text' className='enter-otp'/>
-          <input type='Submit' className='submit-otp'/>
-        </form>
+export default class VerifyEmail extends Component {
+
+  render() {
+    const { data } = this.props.location.state
+    
+    console.log(data);
+    const handleSubmit = (event) => {
+      event.preventDefault()
+      if (data.otp == event.target) {
+        return <SignIn />
+      }
+    }
+    return (
+      <div className='verify-container'>
+        <div className='verify'>
+          <label id='enterOtp'>Enter your OTP: </label>
+          <form className='verify-form'>
+            <input id='enterOtp' type='text' className='enter-otp' />
+            <input type='Submit' className='submit-otp' />
+          </form>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
