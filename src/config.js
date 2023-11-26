d067c4d675ad23c727fda33be6decb850683694c
HaiLeVN <lehai112003@gmail.com>
2023-11-26 11:00:02 +0700

    change config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const REACT_APP_API_URL = 'http://localhost:8080/'
-export const URL_FETCH_AZURE_SERVER = 'http://localhost:8080/'
+export const URL_FETCH_AZURE_SERVER = 'https://zookay.azurewebsites.net/'
 // https://zookay.azurewebsites.net/
 
 // http://localhost:8080/
